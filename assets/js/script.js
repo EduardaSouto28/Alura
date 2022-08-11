605fa17206e3e55f89b2ee8feff3a081beede736
Maria Eduarda Souto <eduardasouto2823@gmail.com>
2022-08-11 15:05:19 -0300

    cartões

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 
-let todasOpcoes = document.querySelectorAll('.menu-lateral__link')
-
 function opcaoSelecionada(opcao) {
 
+    let todasOpcoes = document.querySelectorAll('.menu-lateral__link')
+ 
     for(let i=0; i<todasOpcoes.length; i++){
         todasOpcoes[i].classList.remove("menu-lateral__link--ativo");
     }
-    opcao.classList.add("menu-lateral__link--ativo");
+    opcao.classList.toggle("menu-lateral__link--ativo");
+
+
+}
+
+function ativarMenu(){
+
+    document.querySelector('.menu-lateral').classList.toggle("menu-lateral--ativo");
 
 }
 
