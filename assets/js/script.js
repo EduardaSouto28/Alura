2fd5d7bf26ddcef46e1f441f09aae626587f96d8
Maria Eduarda Souto <eduardasouto2823@gmail.com>
2022-08-15 12:50:15 -0300

    Finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@
 function opcaoSelecionada(opcao) {
 
     let todasOpcoes = document.querySelectorAll('.menu-lateral__link')
- 
-    for(let i=0; i<todasOpcoes.length; i++){
+
+    for (let i = 0; i < todasOpcoes.length; i++) {
         todasOpcoes[i].classList.remove("menu-lateral__link--ativo");
     }
     opcao.classList.toggle("menu-lateral__link--ativo");
@@ -11,9 +11,28 @@ function opcaoSelecionada(opcao) {
 
 }
 
-function ativarMenu(){
+function ativarMenu() {
 
     document.querySelector('.menu-lateral').classList.toggle("menu-lateral--ativo");
 
 }
 
+
+let inicio = document.querySelector(".inicio");
+let picos = document.querySelector(".picos");
+
+
+function abaPicos() {
+    inicio.style.display = "none"
+    picos.style.display = "block"
+    ativarMenu()
+
+}
+
+function abaInicio() {
+    inicio.style.display = "block"
+    picos.style.display = "none"
+    ativarMenu()
+
+}
+abaInicio();
